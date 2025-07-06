08b9dd835a0c6f440f53c09fe17b6713e773ea8a
0x41head <aryangiroud@gmail.com>
2025-07-06 22:13:54 +0530

    Added paint

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,35 @@
 import { useState } from 'react';
 import { Button, Cursor, List, TaskBar } from '@react95/core';
 import { ReaderClosed, WindowsExplorer } from '@react95/icons';
-import viteLogo from '/vite.svg';
-import './App.css';
-
 import '@react95/core/GlobalStyle';
 import '@react95/core/themes/win95.css';
+import WindowSizeProvider from './components/WindowSizeContext';
+import PaintComp from './components/PaintComp';
+import TaskBarComp from './components/TaskBarComponent';
 
 function App() {
+  const [showWindows, setShowWindows] = useState({
+    paint: false,
+    help: false,
+  });
+
+  const toggleWindow = (windowName, isVisible) => {
+    setShowWindows((prev) => ({
+      ...prev,
+      [windowName]: isVisible,
+    }));
+  };
+
+  const handleOpenWindow = (windowName) => toggleWindow(windowName, true);
+  const handleCloseHelp = () => toggleWindow("help", false);
 
   return (
     <>
-
-      <TaskBar
-        list={
-          <List>
-            <List.Item icon={<ReaderClosed variant="32x32_4" />}>
-              Local Disk (C:)
-            </List.Item>
-            <List.Item icon={<WindowsExplorer variant="32x32_4" />}>
-              Windows Explorer
-            </List.Item>
-          </List>
-        }
+    <WindowSizeProvider>
+      <TaskBarComp
+          openPaint={() => handleOpenWindow("paint")}
       />
+    </WindowSizeProvider>
     </>
   );
 }
